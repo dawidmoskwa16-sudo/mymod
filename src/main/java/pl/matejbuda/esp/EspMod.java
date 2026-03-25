16716929cff21e9fc9f14ce704957cd3ef9084f6
dawidmoskwa16-sudo <dawidmoskwa16@gmail.com>
2026-03-25 16:52:23 +0100

    1

--- src/main/java/pl/matejbuda/esp/EspMod.java
+++ src/main/java/pl/matejbuda/esp/EspMod.java
@@ -1,69 +1,113 @@
-// src/main/java/pl/matejbuda/esp/EspMod.java
 package pl.matejbuda.esp;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Box;
 import com.mojang.blaze3d.systems.RenderSystem;
+import org.joml.Matrix4f;
 
 public class EspMod implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        System.out.println("[SimpleESP] Mod załadowany!");
+        
         WorldRenderEvents.AFTER_ENTITIES.register(context -> {
             MinecraftClient mc = MinecraftClient.getInstance();
             if (mc.world == null || mc.player == null) return;
 
             MatrixStack matrices = context.matrixStack();
-            Camera camera = context.camera();
             float tickDelta = context.tickDelta();
+            
+            // Pozycja kamery
+            var cameraPos = context.camera().getPos();
 
-            // wyłączenie testu głębi = widzimy przez ściany
             RenderSystem.disableDepthTest();
+            RenderSystem.enableBlend();
+            RenderSystem.defaultBlendFunc();
+            RenderSystem.setShader(GameRenderer::getPositionColorProgram);
             RenderSystem.lineWidth(2.5f);
 
-            VertexConsumer vertexConsumer = mc.getBufferBuilders()
-                .getEntityVertexConsumers()
-                .getBuffer(RenderLayer.getLines());
+            Tessellator tessellator = Tessellator.getInstance();
+            BufferBuilder buffer = tessellator.begin(VertexFormat.DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR);
 
-            mc.world.getPlayers()
-                .stream()
-                .filter(p -> p != mc.player && p.isAlive() && !p.isSpectator())
-                .forEach(player -> {
-                    double x = player.getLerpedPos(tickDelta).x - camera.getPos().x;
-                    double y = player.getLerpedPos(tickDelta).y - camera.getPos().y;
-                    double z = player.getLerpedPos(tickDelta).z - camera.getPos().z;
+            mc.world.getPlayers().forEach(player -> {
+                if (player == mc.player || !player.isAlive() || player.isSpectator()) return;
 
-                    matrices.push();
-                    matrices.translate(x, y, z);
+                // Interpolowana pozycja gracza
+                double x = player.prevX + (player.getX() - player.prevX) * tickDelta - cameraPos.x;
+                double y = player.prevY + (player.getY() - player.prevY) * tickDelta - cameraPos.y;
+                double z = player.prevZ + (player.getZ() - player.prevZ) * tickDelta - cameraPos.z;
 
-                    Box bb = player.getBoundingBox()
-                        .offset(-player.getX(), -player.getY(), -player.getZ()); // przesunięcie do pozycji względnej
+                matrices.push();
+                matrices.translate(x, y, z);
 
-                    // czerwony outline dla graczy
-                    WorldRenderer.drawBox(
-                        matrices,
-                        vertexConsumer,
-                        bb,
-                        1.0f, 0.0f, 0.0f, 1.0f
-                    );
+                Box box = player.getBoundingBox().offset(-player.getX(), -player.getY(), -player.getZ());
+                Matrix4f matrix = matrices.peek().getPositionMatrix();
 
-                    matrices.pop();
-                });
+                // Rysuj czerwony box
+                drawBox(buffer, matrix, box, 1.0f, 0.0f, 0.0f, 1.0f);
 
-            // rysujemy wszystko naraz na końcu
-            mc.getBufferBuilders().getEntityVertexConsumers().draw(RenderLayer.getLines());
+                matrices.pop();
+            });
 
-            // przywracamy normalne renderowanie
+            BufferRenderer.drawWithGlobalProgram(buffer.end());
+
             RenderSystem.enableDepthTest();
+            RenderSystem.disableBlend();
             RenderSystem.lineWidth(1.0f);
         });
+    }
+
+    private void drawBox(BufferBuilder buffer, Matrix4f matrix, Box box, float r, float g, float b, float a) {
+        float minX = (float) box.minX;
+        float minY = (float) box.minY;
+        float minZ = (float) box.minZ;
+        float maxX = (float) box.maxX;
+        float maxY = (float) box.maxY;
+        float maxZ = (float) box.maxZ;
+
+        // Dolna podstawa
+        buffer.vertex(matrix, minX, minY, minZ).color(r, g, b, a);
+        buffer.vertex(matrix, maxX, minY, minZ).color(r, g, b, a);
+        
+        buffer.vertex(matrix, maxX, minY, minZ).color(r, g, b, a);
+        buffer.vertex(matrix, maxX, minY, maxZ).color(r, g, b, a);
+        
+        buffer.vertex(matrix, maxX, minY, maxZ).color(r, g, b, a);
+        buffer.vertex(matrix, minX, minY, maxZ).color(r, g, b, a);
+        
+        buffer.vertex(matrix, minX, minY, maxZ).color(r, g, b, a);
+        buffer.vertex(matrix, minX, minY, minZ).color(r, g, b, a);
+
+        // Górna podstawa
+        buffer.vertex(matrix, minX, maxY, minZ).color(r, g, b, a);
+        buffer.vertex(matrix, maxX, maxY, minZ).color(r, g, b, a);
+        
+        buffer.vertex(matrix, maxX, maxY, minZ).color(r, g, b, a);
+        buffer.vertex(matrix, maxX, maxY, maxZ).color(r, g, b, a);
+        
+        buffer.vertex(matrix, maxX, maxY, maxZ).color(r, g, b, a);
+        buffer.vertex(matrix, minX, maxY, maxZ).color(r, g, b, a);
+        
+        buffer.vertex(matrix, minX, maxY, maxZ).color(r, g, b, a);
+        buffer.vertex(matrix, minX, maxY, minZ).color(r, g, b, a);
+
+        // Boki (pionowe)
+        buffer.vertex(matrix, minX, minY, minZ).color(r, g, b, a);
+        buffer.vertex(matrix, minX, maxY, minZ).color(r, g, b, a);
+        
+        buffer.vertex(matrix, maxX, minY, minZ).color(r, g, b, a);
+        buffer.vertex(matrix, maxX, maxY, minZ).color(r, g, b, a);
+        
+        buffer.vertex(matrix, maxX, minY, maxZ).color(r, g, b, a);
+        buffer.vertex(matrix, maxX, maxY, maxZ).color(r, g, b, a);
+        
+        buffer.vertex(matrix, minX, minY, maxZ).color(r, g, b, a);
+        buffer.vertex(matrix, minX, maxY, maxZ).color(r, g, b, a);
     }
 }
